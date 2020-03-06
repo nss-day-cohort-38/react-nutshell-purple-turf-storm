29a1d20377b6542306ec1ef4bf377d747c43b810
Roxanne Nasraty <rsnasraty@gmail.com>
2020-03-06 14:58:32 -0600

    added api

---
+++ src/components/Api/Api.js
@@ -0,0 +1,59 @@
+{
+    "messages": [
+        {
+            "id": 1,
+            "userId": 1,
+            "message": "Hello, World",
+            "date": "2020-3-6"
+        }
+    ],
+    "tasks": [
+        {
+            "id": 1,
+            "userId": 1,
+            "name": "Start on individual tickets",
+            "isComplete": false,
+            "completeBy": "2020-3-11"
+        }
+    ],
+    "articles": [
+        {
+            "id": 1,
+            "userId": 1,
+            "title": "Corona Virus",
+            "synopsis": "Scary shit",
+            "url": "https://scaryshit.com",
+            "date": "2020-3-2"
+        }
+    ],
+    "events": [
+        {
+            "id": 1,
+            "userId": 1,
+            "name": "Capstone sprint",
+            "date": "2020-3-12",
+            "location": "NSS"
+        }
+    ],
+    "friends": [
+        {
+            "id": 1,
+            "userId": 1,
+            "currentUserId": 2
+        }
+    ],
+    "users": [
+        {
+            "id": 1,
+            "userName": "BillyBobSoftwareSlayer",
+            "password": "shumulungdong",
+            "email": "Slayer93@yahoo.com"
+        },
+        {
+            "id": 2,
+            "userName": "JohnJacobJinkleHymerSchmitt",
+            "password": "recess",
+            "email": "hisNameIsMyNameToo@Gmail.com"
+        }
+    ]
+}
