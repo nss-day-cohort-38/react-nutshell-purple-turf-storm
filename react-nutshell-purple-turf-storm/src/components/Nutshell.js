f53fa24b2a5c06472c5a6a0b53c4b73b0222e1c6
Matthew Kroeger <mattk932009@gmail.com>
2020-03-06 10:44:47 -0600

    navbar stuff

---
+++ react-nutshell-purple-turf-storm/src/components/Nutshell.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+import "./Nutshell.css"
+
+const Nutshell = () => {
+    const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
+
+    const [hasUser, setHasUser] = useState(isAuthenticated());
+
+    const setUser = user => {
+        sessionStorage.setItem("credentials", JSON.stringify(user));
+        setHasUser(isAuthenticated());
+    }
+
+    const clearUser = () => {
+        sessionStorage.clear();
+        setHasUser(isAuthenticated());
+    }
+
+    return (
+    <>
+        <NavBar hasUser={hasUser} clearUser={clearUser} />
+        <ApplicationViews hasUser={hasUser} setUser={setUser} />
+    </>
+    )
+}
+
+export default Nutshell
