9892d1b1e9b49ab0003c30dd488b842d62fbcbf0
Roxanne Nasraty <rsnasraty@gmail.com>
2020-03-06 13:54:36 -0600

    Had to make a new branch, added Home basic html

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+ 
+
+  return (
+    <>
+    <div> Home </div>
+    <h1> Purple Turf Storm</h1>
+    <h2> Welcome to our website! </h2>
+     <img src="https://cache.desktopnexus.com/thumbseg/2426/2426241-bigthumbnail.jpg"></img>   
+    </>
+  );
+};
+
+export default Home;
