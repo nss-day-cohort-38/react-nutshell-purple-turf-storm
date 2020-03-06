4fcabe7fe47dc19209dd942fa81978dc3ac3aacc
Matt Reeds <mattlreeds@gmail.com>
2020-03-06 13:26:45 -0600

    base application views routes created

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,21 @@
+import { Route, Redirect } from "react-router-dom";
+import React from "react";
+import Home from "./home/Home";
+import Login from "./auth/Login";
+
+const ApplicationViews = props => {
+    const setUser = props.setUser;
+    const hasUser = props.hasUser;
+    return (
+        <React.Fragment>
+            <Route path="/login" render={props => {
+                return <Login setUser={setUser} {...props}/>
+            }}
+            />
+            <Route exact="/" render={props => {
+                return <Home />
+            }}
+            />
+        </React.Fragment>
+    )
+}
