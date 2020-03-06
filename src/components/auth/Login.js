24e5a345ec642400b1d6249b1f6cecb28e54f7a8
Matthew Kroeger <mattk932009@gmail.com>
2020-03-06 14:51:49 -0600

    formatting change

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,50 +1,63 @@
-import React, { useState } from "react"
-import "../Nutshell.css"
-import UserManager from "../../modules/UserManager"
+import React, { useState } from "react";
+import "../Nutshell.css";
+import UserManager from "../../modules/UserManager";
 
 const Login = props => {
-  const [credentials, setCredentials] = useState({ userName: "", password: "" });
+  const [credentials, setCredentials] = useState({
+    userName: "",
+    password: ""
+  });
 
   // Update state whenever an input field is edited
-  const handleFieldChange = (evt) => {
+  const handleFieldChange = evt => {
     const stateToChange = { ...credentials };
     stateToChange[evt.target.id] = evt.target.value;
     setCredentials(stateToChange);
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = e => {
     e.preventDefault();
     let valid = false;
     UserManager.getAll().then(users => {
-        users.filter(user => {
-            if(user.userName === credentials.userName && user.password === credentials.password) {
-                valid = true;
-                props.setUser(credentials)
-                props.history.push("/");
-            } 
-        })
-        if(!valid) {
-            alert("incorrect username/password")
+      users.filter(user => {
+        if (
+          user.userName === credentials.userName &&
+          user.password === credentials.password
+        ) {
+          valid = true;
+          props.setUser(credentials);
+          props.history.push("/");
         }
-    })
-  }
+      });
+      if (!valid) {
+        alert("incorrect username/password");
+      }
+    });
+  };
 
   return (
     <form onSubmit={handleLogin}>
       <fieldset>
         <h3>Please sign in</h3>
         <div className="formgrid">
-          <input onChange={handleFieldChange} type="userName"
-            id="userName"
-            placeholder="UserName"
-            required="" autoFocus="" />
           <label htmlFor="inputUserName">Username</label>
+          <input
+            onChange={handleFieldChange}
+            type="userName"
+            id="userName"
+            placeholder="Username"
+            required=""
+            autoFocus=""
+          />
 
-          <input onChange={handleFieldChange} type="password"
+          <label htmlFor="inputPassword">Password</label>
+          <input
+            onChange={handleFieldChange}
+            type="password"
             id="password"
             placeholder="Password"
-            required="" />
-          <label htmlFor="inputPassword">Password</label>
+            required=""
+          />
         </div>
         <button type="submit">Sign in</button>
       </fieldset>
