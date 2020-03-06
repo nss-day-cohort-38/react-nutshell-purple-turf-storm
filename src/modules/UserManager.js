22b3421b2676472d87aa4039f0ec114297e03d99
Matthew Kroeger <mattk932009@gmail.com>
2020-03-06 13:32:18 -0600

    prep for merge

---
+++ src/modules/UserManager.js
@@ -0,0 +1,33 @@
+const remoteURL = "http://localhost:5002";
+
+export default {
+  get(id) {
+    return fetch(`${remoteURL}/animals/${id}`).then(result => result.json());
+  },
+  getAll() {
+    return fetch(`${remoteURL}/animals`).then(result => result.json());
+  },
+  post(newAnimal) {
+    return fetch(`${remoteURL}/animals`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newAnimal)
+    }).then(data => data.json());
+  },
+  delete(id) {
+    return fetch(`${remoteURL}/animals/${id}`, {
+      method: "DELETE"
+    }).then(result => result.json());
+  },
+  update(editedAnimal) {
+    return fetch(`${remoteURL}/animals/${editedAnimal.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedAnimal)
+    }).then(data => data.json());
+  }
+};
