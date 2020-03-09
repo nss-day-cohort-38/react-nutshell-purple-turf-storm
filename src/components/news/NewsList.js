008e51f6524b4632cbfb08a5f8dda1099f760ed4
Matthew Kroeger <mattk932009@gmail.com>
2020-03-09 12:22:25 -0500

    added list

---
+++ src/components/news/NewsList.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import NewsCard from "./NewsCard";
+import NewsManager from "../../modules/NewsManager";
+
+const NewsList = (props) => {
+  const [articles, setArticles] = useState([]);
+
+  const getNews = () => {
+    return NewsManager.getAll().then(allNews => {
+      setArticles(allNews);
+    });
+  };
+
+  const deleteArticle = id => {
+    NewsManager.delete(id).then(() =>
+      NewsManager.getAll().then(setArticles)
+    );
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <>
+    {/* <section className="section-content">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => {
+            props.history.push("/articles/new");
+          }}
+        >
+          Post News
+        </button>
+      </section> */}
+    <div className="container-cards">
+      {articles.map(article => (
+        <NewsCard
+          key={article.id}
+          article={article}
+          deleteArticle={deleteArticle}
+          {...props}
+        />
+      ))}
+    </div>
+    </>
+  );
+};
+export default NewsList;
